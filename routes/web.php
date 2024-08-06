cac1f6c7458d6dd0cdc4a731fa6708382a4396f5
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-08-06 22:54:44 +0900

    勘定科目マスタ

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Transaction\Create as TransactionCreate;
 use App\Livewire\Transaction\Edit as TransactionEdit;
 use App\Livewire\Transaction\Show as TransactionShow;
+use App\Livewire\Account\Show as AccountShow;
 
 Route::get('/', function() {
     return redirect(route('dashboard'));
@@ -17,6 +18,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('transaction/create', TransactionCreate::class)->name('transaction.create');
     Route::get('transaction/edit/{id}', TransactionEdit::class)->name('transaction.edit');
     Route::get('transaction/{month?}', TransactionShow::class)->name('transaction.show');
+
+    Route::get('account', AccountShow::class)->name('account.show');
 });
 
 require __DIR__ . '/auth.php';

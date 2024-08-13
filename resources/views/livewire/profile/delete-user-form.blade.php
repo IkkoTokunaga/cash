e53cc14995f351bf1550b67c66f5fc5f16d8f4be
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-08-13 22:27:23 +0900

    アカウント更新関連のレイアウト

--- resources/views/livewire/profile/delete-user-form.blade.php
+++ resources/views/livewire/profile/delete-user-form.blade.php
@@ -26,28 +26,28 @@ new class extends Component
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            {{ __('アカウント削除') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('アカウントを削除すると関連する全てのデータが削除されます。削除には最新の注意をお願いいたします。') }}
         </p>
     </header>
 
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('削除を実行する') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable>
         <form wire:submit="deleteUser" class="p-6">
 
             <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('本当に削除してもよろしいですか?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('アカウントを削除すると関連する全てのデータが削除されます。') }}
             </p>
 
             <div class="mt-6">
@@ -71,7 +71,7 @@ new class extends Component
                 </x-secondary-button>
 
                 <x-danger-button class="ms-3">
-                    {{ __('Delete Account') }}
+                    {{ __('削除されました') }}
                 </x-danger-button>
             </div>
         </form>
--- resources/views/livewire/profile/update-password-form.blade.php
+++ resources/views/livewire/profile/update-password-form.blade.php
@@ -6,8 +6,7 @@ use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 use Livewire\Volt\Component;
 
-new class extends Component
-{
+new class extends Component {
     public string $current_password = '';
     public string $password = '';
     public string $password_confirmation = '';
@@ -41,38 +40,58 @@ new class extends Component
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
+            {{ __('パスワード更新') }}
         </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
     </header>
 
     <form wire:submit="updatePassword" class="mt-6 space-y-6">
         <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input wire:model="current_password" id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
+            <table class="table table-bordered mt-2 ">
+                <tr>
+                    <th class="table-success">
+                        <x-input-label for="update_password_current_password" :value="__('現在のパスワード')" />
+                    </th>
+                    <td>
+                        <x-text-input wire:model="current_password" id="update_password_current_password"
+                            name="current_password" type="password" class="mt-1 block w-full"
+                            autocomplete="current-password" />
+                        <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
+                    </td>
+                </tr>
+                <tr>
+                    <th class="table-success">
+                        <x-input-label for="update_password_password" :value="__('新しいパスワード')" />
+                    </th>
+                    <td>
+                        <x-text-input wire:model="password" id="update_password_password" name="password"
+                            type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </td>
+                </tr>
+                <tr>
+                    <th class="table-success">
+                        <x-input-label for="update_password_password_confirmation" :value="__('確認')" />
+                    </th>
+                    <td>
+                        <x-text-input wire:model="password_confirmation" id="update_password_password_confirmation"
+                        name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                            </td>
+                </tr>
+            </table>
         </div>
 
         <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input wire:model="password" id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input wire:model="password_confirmation" id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <button type="submit" class="btn btn-danger btn-lg mt-3">更新</button>
 
             <x-action-message class="me-3" on="password-updated">
-                {{ __('Saved.') }}
+                {{ __('更新しました') }}
             </x-action-message>
         </div>
     </form>

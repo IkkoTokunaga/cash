d19d389fc7bd45877116a97815c8418ecf21df92
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-08-27 00:04:23 +0900

    スマホの一覧画面を修正

--- resources/views/livewire/transaction/show.blade.php
+++ resources/views/livewire/transaction/show.blade.php
@@ -37,20 +37,20 @@ use Carbon\Carbon;
                         <thead class="table-success">
                             <tr>
                                 <th class="text-center">日付</th>
-                                <th class="text-center">科目</th>
+                                <th class="text-center hidden md:table-cell">科目</th>
                                 <th class="text-center">摘要</th>
                                 <th class="text-center hidden md:table-cell">収入 (円)</th>
                                 <th class="text-center table-cell md:hidden">金額 (円)</th>
                                 <th class="text-center hidden md:table-cell">支出 (円)</th>
                                 <th class="text-center">残高 (円)</th>
-                                <th class="text-center">削除</th>
+                                <th class="text-center"></th>
                             </tr>
                         </thead>
                         <tbody>
                             @if ($prevMonthData)
                                 <tr>
                                     <td class="text-center">{{ Carbon::parse($prevEndOfMonth_Ymd)->format('n/j') }}</td>
-                                    <td></td>
+                                    <td class="hidden md:block"></td>
                                     <td class="text-primary">前月繰越金</td>
                                     <td></td>
                                     <td class="hidden md:block"></td>
@@ -80,16 +80,22 @@ use Carbon\Carbon;
                                 <tr @if (session('saved_transaction_id') === $transaction->id && session('saved_transaction_id')) class="table-info" @endif
                                     style="cursor: pointer;">
                                     <td class="text-center" wire:click="edit({{ $transaction->id }})">
-                                        {{ $transaction->date }}</td>
-                                    <td wire:click="edit({{ $transaction->id }})">{{ $transaction->account_name }}
+                                        {{ $transaction->date }}
                                     </td>
-                                    <td wire:click="edit({{ $transaction->id }})">{{ $transaction->description }}</td>
+                                    <td wire:click="edit({{ $transaction->id }})" class="text-end hidden md:table-cell">
+                                        {{ $transaction->account_name }}
+                                    </td>
+                                    <td wire:click="edit({{ $transaction->id }})">
+                                        <span class="md:hidden">
+                                            【{{ $transaction->account_name }}】<br>
+                                        </span>
+                                        {{ $transaction->description }}
+                                    </td>
                                     <td class="text-end" wire:click="edit({{ $transaction->id }})">
                                         {{ $transaction->income != 0 ? number_format($transaction->income) : '' }}
                                         <span class="md:hidden">
                                             {{ $transaction->expense != 0 ? '-' . number_format($transaction->expense) : '' }}
                                         </span>
-
                                     </td>
                                     <td class="text-end hidden md:table-cell"
                                         wire:click="edit({{ $transaction->id }})">
@@ -103,14 +109,14 @@ use Carbon\Carbon;
                                                 <div wire:click="delete({{ $transaction->id }})"
                                                     wire:confirm="削除されたデータは元に戻せません。よろしいですか?" style="cursor: pointer;">
                                                     <img src="{{ asset('storage/delete_24dp_666666_FILL0_wght400_GRAD0_opsz24.svg') }}"
-                                                        alt="" class="min-w-[26px]">
+                                                        alt="削除" class="min-w-[30px]">
                                                 </div>
                                             </div>
                                         @else
                                             <div class="d-flex justify-center">
                                                 <i style="cursor: not-allowed;"><img
                                                         src="{{ asset('storage/delete_24dp_666666_FILL0_wght400_GRAD0_opsz24.svg') }}"
-                                                        alt="" class="min-w-[26px]"></i>
+                                                        alt="削除" class="min-w-[30px]"></i>
                                             </div>
                                         @endif
                                     </td>
@@ -118,7 +124,8 @@ use Carbon\Carbon;
                             @endforeach
                             @if (count($transactions))
                                 <tr>
-                                    <th colspan="3" class="table-success">合計</th>
+                                    <th colspan="3" class="table-success hidden md:table-cell">合計</th>
+                                    <th colspan="2" class="table-success md:hidden">合計</th>
                                     <td class="text-end hidden md:table-cell">
                                         {{ $totalIncome != 0 ? number_format($totalIncome) : '' }}</td>
                                     <td class="text-end md:hidden">

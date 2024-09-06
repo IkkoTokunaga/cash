dc372ecb47bdd4cc4083d7764faa6feb46df1db0
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-09-06 16:49:55 +0900

    必須の色を調整

--- resources/views/components/required-label.blade.php
+++ resources/views/components/required-label.blade.php
@@ -1,4 +1,4 @@
-<span {{ $attributes->merge(['class' => 'text-xs font-semibold bg-red-800 text-white px-2 py-1 ']) }}>
+<span {{ $attributes->merge(['class' => 'text-xs font-semibold bg-rose-500 text-white px-2 py-1 ']) }}>
 必須
 </span>
 

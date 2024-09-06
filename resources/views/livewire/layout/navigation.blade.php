bb67207b19e32d0c953e1012f9fb43e6ea4d4fe5
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-09-06 16:55:09 +0900

    navのレスポンシブ閾値修正

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -17,7 +17,7 @@ new class extends Component {
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -27,48 +27,48 @@ new class extends Component {
                     </a>
                 </div>
 
-                <div class="sm:hidden space-x-8 -my-px ms-5 flex">
+                <div class="md:hidden space-x-8 -my-px ms-5 flex">
                     <x-nav-link :href="route('transaction.show')" :active="request()->routeIs('transaction.show')" wire:navigate>
                         {{ __('収支') }}<br>{{ __('一覧') }}
                     </x-nav-link>
                 </div>
-                <div class="sm:hidden space-x-8 -my-px ms-5 flex">
+                <div class="md:hidden space-x-8 -my-px ms-5 flex">
                     <x-nav-link :href="route('transaction.create')" :active="request()->routeIs('transaction.create')" wire:navigate>
                         {{ __('新規') }}<br>{{ __('作成') }}
                     </x-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('transaction.show')" :active="request()->routeIs('transaction.show')" wire:navigate>
                         {{ __('収支一覧') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('transaction.create')" :active="request()->routeIs('transaction.create')" wire:navigate>
                         {{ __('新規作成') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('account.show')" :active="request()->routeIs('account.show')" wire:navigate>
                         {{ __('勘定科目') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('target_user.show')" :active="request()->routeIs('target_user.show')" wire:navigate>
                         {{ __('取引先') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('profile')" :active="request()->routeIs('profile')" wire:navigate>
                         {{ __('ユーザ情報') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link :href="route('support.create')" :active="request()->routeIs('support.create')" wire:navigate>
                         {{ __('問い合わせ') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
                     <x-nav-link wire:click="logout" wire:confirm="ログアウトしてもよろしいですか?" style="cursor: pointer">
                         {{ __('ログアウト') }}
                     </x-nav-link>
@@ -76,7 +76,7 @@ new class extends Component {
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden md:flex md:items-center md:ms-6">
                 <div align="right" width="48">
                     <x-slot name="trigger">
                         <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name"
@@ -87,7 +87,7 @@ new class extends Component {
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center md:hidden">
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -103,7 +103,7 @@ new class extends Component {
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden md:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('ホーム') }}
@@ -150,7 +150,7 @@ new class extends Component {
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800" x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name"
                     x-on:profile-updated.window="name = $event.detail.name"></div>
-                {{-- <div class="font-medium text-sm text-gray-500">{{ auth()->user()->email }}</div> --}}
+                {{-- <div class="font-medium text-md text-gray-500">{{ auth()->user()->email }}</div> --}}
             </div>
 
         </div>

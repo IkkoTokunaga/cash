6135eed02d15e8537452255d6a522b24914daceb
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-09-09 15:56:24 +0900

    HTML修正

--- resources/views/livewire/document/target_user_edit.blade.php
+++ resources/views/livewire/document/target_user_edit.blade.php
@@ -6,7 +6,7 @@
                 <span
                     class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xl sm:text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 mb-2">
                     新規取引先作成
-                    </a>
+                    </span>
 
                     <table class="table table-bordered mt-2">
                         <thead class="table-success">

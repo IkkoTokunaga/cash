3d050cbd9f5071d37a0c85bdbdfbf0112a06c0b0
ikko tokunaga <ikko-tokunaga@emdes.co.jp>
2024-09-14 23:28:43 +0900

    締め処理のバリデーション不具合を修正

--- app/Providers/ValidatorServiceProvider.php
+++ app/Providers/ValidatorServiceProvider.php
@@ -41,7 +41,7 @@ class ValidatorServiceProvider extends ServiceProvider
         Validator::extend(
             'is_prev_monthly_closing',
             function ($attribute, $value, $parameters, $validator) {
-                $prevEndOfMonth = Carbon::parse($value)->subMonth()->format('Y-m-t');
+                $prevEndOfMonth = Carbon::parse($value)->subMonthNoOverflow()->format('Y-m-t');
                 $isMonthlyClosing = Summary::get($prevEndOfMonth);
                 // 初回は保存できないため、初回の締め処理は例外
                 if (!$isMonthlyClosing) {
